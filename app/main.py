7847b2fae24c45aa3c5c24baa7eaa6c60b08e960
Nimisha Nandakumar <nimishanandakumar777@gmail.com>
2024-01-10 15:15:35 +0000

    Webpage first phase completed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from website import create_app
+from flask import render_template
 
 app = create_app()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host="0.0.0.0")
--- app/website/search.py
+++ app/website/search.py
@@ -1,3 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template,request
+import pandas as pd
 
 search = Blueprint('search', __name__)
+movies = pd.read_parquet('df_name.parquet')
+
+
+def html_block_template(href, name):
+    return f'<li><a class="dropdown-item" href="{href}" target="_blank">{name}</a></li>'
+
+
+@search.route('/search', methods=['GET'])
+def query():
+    query = request.args.get('query', '')
+
+    results_html = ''
+
+    filtered_movies = movies[movies['name'].str.contains(query, case=False, na=False)]
+    
+
+    for d in filtered_movies.values:
+        results_html += html_block_template(f'detail?id={d[0]}', d[1])
+
+    return results_html
